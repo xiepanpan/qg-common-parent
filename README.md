97f04d5650d7577c6dc309fc4da58220fa742258
29081 <xiepanpan1995@163.com>
2019-10-04 14:39:41 +0800

    登录注销

--- README.md
+++ README.md
@@ -4,3 +4,8 @@
 
 
 用户校验
+
+命令
+```linux
+docker run -d -p 9010:9000 -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer
+```
--- qg-common-dto/src/main/java/com/qg/dto/ReturnResultUtils.java
+++ qg-common-dto/src/main/java/com/qg/dto/ReturnResultUtils.java
@@ -1,30 +1,39 @@
 package com.qg.dto;
 
-import com.qg.dto.ReturnResult;
-
-/**
- * @author: xiepanpan
- * @Date: 2019/10/3
- * @Description: 统一返回工具类
+/***
+ * created by 北大课工场
+ *
+ * 统一返回工具类
  */
-public class ReturnResultUtils {
+public class ReturnResultUtils{
 
-    /**
-     * 返回成功
+    /***
+     * 成功 不带数据
      * @return
      */
-    public static ReturnResult returnSuccess(Object data) {
-        ReturnResult returnResult = new ReturnResult();
+    public static ReturnResult returnSuccess(){
+        ReturnResult returnResult=new ReturnResult();
+        returnResult.setCode(0);
+        return returnResult;
+    }
+    /***
+     * 成功 不带数据
+     * @return
+     */
+    public static ReturnResult returnSuccess(Object data){
+        ReturnResult returnResult=new ReturnResult();
         returnResult.setCode(0);
         returnResult.setData(data);
         return returnResult;
     }
-
-    public static ReturnResult returnFail(Integer code,String message) {
-        ReturnResult returnResult = new ReturnResult();
+    /***
+     * 失败
+     * @return
+     */
+    public static ReturnResult returnFail(Integer code,String message){
+        ReturnResult returnResult=new ReturnResult();
         returnResult.setCode(code);
         returnResult.setMessage(message);
         return returnResult;
     }
-
 }
